0f85f8422f553a431373990a5af45436c386840d
cineron <cineron+gh@gmail.com>
2018-09-06 22:34:28 -0500

    following along with the Roderick video. forgot to commit earlier.

---
+++ js/classes.js
@@ -0,0 +1,74 @@
+// from the Roderick follow along
+
+class Entity {
+    constructor() {
+        this.sprite = "images/";
+        this.x = 1;
+        this.y = 5;
+    }
+
+    update(dt) {
+        this.isOutOfBoundsX = this.x > 5;
+        this.isOutOfBoundsY = this.y < 1;
+    }
+
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x * 100, this.y * 80);
+    }
+
+    checkCollisions(playerOrEnemy){
+        if (this.y === playerOrEnemy.y){
+            if (this.x >= playerOrEnemy.x - 0.74 && this.x <= playerOrEnemy.x + 0.55){
+                return true;
+            }
+        } else {
+            return false;
+        }
+    }
+}
+
+class Player extends Entity {
+    constructor() {
+        super();
+        this.sprite += "char-boy.png";
+    }
+
+    handleInput(input) {
+        switch (input) {
+            case 'left':
+                this.x = this.x > 0 ? this.x - 1 : this.x;
+                break;
+            case 'up':
+                this.y = this.y > 0 ? this.y - 1 : this.y;
+                break;
+            case 'right':
+                this.x = this.x < 4 ? this.x + 1 : this.x;
+                break;
+            case 'down':
+                this.y = this.y < 5 ? this.y + 1 : this.y;
+                break;
+        
+            default:
+                break;
+        }
+    }
+}
+
+class Enemy extends Entity {
+    constructor(x, y) {
+        super();
+        this.sprite += "enemy-bug.png";
+        this.x = x;
+        this.y = y;
+    }
+
+    update(dt){
+        super.update();
+        if(this.isOutOfBoundsX){
+            this.x = -1;
+        } else {
+            this.x += dt;
+        }
+
+    }
+}
